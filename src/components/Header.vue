23b806ea6bfbb647bd65718576387f9704a27bdd
Natalia <natalia-321s@outlook.com>
2021-01-27 20:39:51 -0300

    Estruturando o projeto

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header"></header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+.header{
+    width: 100%;
+    background: var(--color-purple);
+    height: 4.5rem;
+}
+</style>
---
+++ src/initalCards.js
@@ -0,0 +1,17 @@
+// Dados para testar a aplicação
+export default {
+  backlog: [
+    { id: 1, text: "Tarefa 1 - Manutenção" },
+    { id: 2, text: "Tarefa 2 - Refatoração de tela" },
+    { id: 3, text: "Tarefa 3 - Refatoração de tela de aplicativo" },
+  ],
+  dev: [
+    { id: 4, text: "Crud" },
+    { id: 5, text: "Dashboard" },
+  ],
+  test: [
+    { id: 6, text: "Testes do aplicativo" },
+    { id: 7, text: "Testes do back-end" },
+    { id: 8, text: "Testes do front-end" },
+  ]
+}
