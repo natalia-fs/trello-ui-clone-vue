43ae19a900c293a36c12e495f93bc634994b5368
Natalia <natalia-321s@outlook.com>
2021-01-27 23:32:51 -0300

    handleDrop e placeholder

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,9 @@
         <Container
           group-name="list"
           @drag-start="handleDragStart('backlog', $event)"
-          @drop="handleDrop"
+          @drop="handleDrop('backlog', $event)"
           :get-child-payload="getChildPayload"
+          :drop-placeholder="{className: 'placeholder'}"
         >
           <Draggable v-for="card in cards.backlog" :key="card.id">
             <Card>{{card.text}}</Card>
@@ -20,8 +21,9 @@
         <Container
           group-name="list"
           @drag-start="handleDragStart('dev', $event)"
-          @drop="handleDrop"
+          @drop="handleDrop('dev', $event)"
           :get-child-payload="getChildPayload"
+          :drop-placeholder="{className: 'placeholder'}"
         >
           <Draggable v-for="card in cards.dev" :key="card.id">
             <Card>{{card.text}}</Card>
@@ -33,8 +35,9 @@
         <Container
           group-name="list"
           @drag-start="handleDragStart('tests', $event)"
-          @drop="handleDrop"
+          @drop="handleDrop('tests', $event)"
           :get-child-payload="getChildPayload"
+          :drop-placeholder="{className: 'placeholder'}"
         >
           <Draggable v-for="card in cards.tests" :key="card.id">
             <Card>{{card.text}}</Card>
@@ -46,8 +49,9 @@
         <Container
           group-name="list"
           @drag-start="handleDragStart('done', $event)"
-          @drop="handleDrop"
+          @drop="handleDrop('done', $event)"
           :get-child-payload="getChildPayload"
+          :drop-placeholder="{className: 'placeholder'}"
         >
           <Draggable v-for="card in cards.done" :key="card.id">
             <Card>{{card.text}}</Card>
@@ -100,8 +104,19 @@ export default {
       }
       console.log(this.draggingCard)
     },
-    handleDrop(){
-
+    handleDrop(lane, dropResult){
+      console.log(dropResult, lane)
+      const {removedIndex, addedIndex } = dropResult;
+      if(lane === this.draggingCard.lane && removedIndex === addedIndex){
+        return;
+      }
+      if(removedIndex !== null){
+        this.cards[lane].splice(removedIndex, 1);
+      }
+      if(addedIndex !== null){
+        this.cards[lane].splice(addedIndex, 0, this.draggingCard.cardData)
+      }
+      console.log(lane, dropResult, removedIndex, addedIndex)
     },
     getChildPayload(index){
       return index;
@@ -114,12 +129,12 @@ export default {
 .board{
   display: flex;
   margin: 1.2rem .8rem;
+  align-items: flex-start;
 }
 
 .lane{
   background: var(--color-grey);
   width: 23rem;
-  height: 30rem;
   border-radius: .8rem;
   box-shadow: 0 .1rem .2rem 0 rgba(33, 33, 33, 0.1);
   padding: 0 .7rem;
@@ -129,6 +144,13 @@ export default {
 .lane-title{
   padding: .8rem .5rem;
   margin-bottom: .6rem;
-
 }
+
+.placeholder {
+  background: rgba(33, 33, 33, .08);
+  border-radius: .4rem;
+  transform: scaleY(0.85);
+  transform-origin: 0% 0%;
+}
+
 </style>
